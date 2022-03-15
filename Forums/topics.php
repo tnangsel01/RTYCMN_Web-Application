14f97246179909eaa527d16776dcca6fcab0e103
Andrew-LeXiong <80724217+Andrew-LeXiong@users.noreply.github.com>
2022-03-14 19:56:29 -0500

    LinkingThePagesTogether
    Just linked the pages together

---
+++ Forums/topics.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../styleSheets/styleSheet.css">
+</head>
+<body>
+    <div class="topnav">
+        <a  href="../homepage.php">Home</a>
+        <a href="../Events.php">Events</a>
+        <a href="../dontation.php">donations</a>
+        <a href="../membership.php">membership</a>
+        <a href="../store.php">store</a>
+        <a href="#Fourms">Forums</a>
+
+    </div>
+    <p>Welcome to the Topics page<p>
+
+
+
+    <footer>
+    <div><h4>Connect with RTYCMN</h4>
+    <a href="#facebook" target="_blank" class="fb"><img src="../Images/facebookLogo.jpg" width='30' height='30'></a>
+        &nbsp;<a href="#twitter" target="_blank" class="tw"><img src="../Images/twitter.jpg" width='30' height='30'></a>
+        &nbsp;<a href="#images" target="_blank" class="yt"><img src="../Images/youtube.jpg" width='30' height='30'></a>
+        &nbsp;<a href="#insta" target="_blank" class="ig"><img src="../images/instagram.jpg" width='30' height='30'></a>
+    </div>
+</footer>
+</body>
+</html> 
