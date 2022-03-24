cb637d7f675474cf0334fa6ded21032eb3435873
tnangsel01 <tenzin.nangsel@my.metrostate.edu>
2022-03-23 19:25:06 -0500

    login uodated
    see login

--- login/login.php
+++ login/login.php
@@ -1,14 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <title>Login</title> 
 </head>
+<body>
+<!-- <?php include("header.php"); ?> -->
+<div class="text-center">
+    <form style="max-width: 300px; margin:auto;" action="" method="post">
+        <img class="mt-4 mb-4" src="login-icon-images.PNG" height="72" alt="login logo">
+        <h1 class="h3 mb-3 font-weight-normal">Please sign In</h1>
+        
+        <label for="emailAddress" class="sr-only" type="hidden"></label>
+        <input type="email" name="email" id="emailAddress" placeholder="Email Address" class="form-control" required autofocus>
+        
+       
+        <label for="password" class="sr-only" type="hidden"></label>
+        <input type="password" name="password" id="password" placeholder="Password" class="form-control" required autofocus>
+        
+        <div class="mt-3">
+            <button class="btn btn-lg btn-success btn-block">Sign In</button> 
+        </div>
+    </form>
 
-<?php include("header.php"); ?>
-<p style="text-align: center;"><a style="color: white; text-decoration: none; background-color: green; cursor: pointer; border: 1px solid #007bff; display: inline-block; font-weight: 400; text-align: center; white-space: nowrap; vertical-align: middle; padding: .375rem .75rem; font-size: 1rem; line-height: 1.5; border-radius: .25rem; transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;" href="../login/adminLogin.php" class="btn btn-primary" role="button" title="Sign on with StarID Single Sign-On">Admin Login</a></p>
-<p style="text-align: center;"><a style="color: white; text-decoration: none; background-color: green; cursor: pointer; border: 1px solid #007bff; display: inline-block; font-weight: 400; text-align: center; white-space: nowrap; vertical-align: middle; padding: .375rem .75rem; font-size: 1rem; line-height: 1.5; border-radius: .25rem; transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;" href="../login/userLogin.php" class="btn btn-primary" role="button" title="Sign on with StarID Single Sign-On">Member Login</a></p>
-
-
-
-<?php include("footer.php"); ?>
+</div>
+<!-- <?php include("footer.php"); ?> -->
+</body>
 </html>
