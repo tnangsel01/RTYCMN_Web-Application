f5e6f1f587dfdd754f461520af47156b0baeeeee
tnangsel01 <tenzin.nangsel@my.metrostate.edu>
2022-03-28 18:12:23 -0500

    last update before submit

--- rtycmn/login.php
+++ rtycmn/login.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <title>Login</title> 
 </head>
 <body>
 <?php include("header.php"); ?>
+
+<?php
+$email = 
 <div class="text-center">
-    <form style="max-width: 300px; margin:auto;" action="" method="post">
+    <form style="max-width: 300px; margin:auto;" action="<?php echo htmlspecialchars($_SERVER["" method="post">
         <img class="mt-8 mb-8" src="login-icon-images.PNG" height="102" alt="login logo">
         <h1 class="h3 mb-3 font-weight-normal">Please sign In</h1>
         
