60ac3c202fd01a5920f6779421f2a9e01aed6a6a
tnangsel01 <tenzin.nangsel@my.metrostate.edu>
2022-03-28 16:48:03 -0500

    updated all files
    design updated with all other files.

---
+++ rtycmn/rtycmn/forumCreate.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Forum</title>
+</head>
+<?php include("header.php"); ?>
+
+<div class="container pt-5">  
+    <div class="text-center">
+        <form style="max-width: 550px; margin:auto;" method='post' action=''>
+            <label for="category" class="sr-only"><strong>Category name:</strong> </label>
+            <input class="form-control" type='text' name='cat_name' required><br>
+            <label for="category" class="sr-only"><strong>Category description:</strong></label>
+            <textarea class="form-control" name='cat_description' rows="8" cols="40"></textarea>
+            
+            <div class="mt-3">
+                <button class="btn btn-lg btn-success btn-block">Add Category</button> 
+            </div>
+        </form>     
+    </div>
+</div>
+
+<?php include("footer.php"); ?>
+</html>
