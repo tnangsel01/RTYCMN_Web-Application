90e314c3c8546870a2bc855c055861bf7bf93144
tnangsel01 <79672177+tnangsel01@users.noreply.github.com>
2022-04-04 17:31:48 -0500

    login register is linked

--- rtycmn/register.php
+++ rtycmn/register.php
@@ -11,7 +11,7 @@
     <div class="container pt-5">  
         <div class="text-center">    
         <div class="signup-form">
-            <form style="max-width: 450px; margin:auto;" action="register_a.php" method="post" enctype="multipart/form-data">
+            <form style="max-width: 450px; margin:auto;" action="login_register.php" method="post" enctype="multipart/form-data">
                 <h2>Create your Account</h2>
                 <div class="form-group p-2">
                     <div class="row">
@@ -23,10 +23,7 @@
                     <input type="email" class="form-control" name="email" placeholder="Email" required="required">
                 </div>
                 <div class="form-group p-2">
-                    <input type="tel" class="form-control" name="phone" placeholder="Phone Number">
-                </div>
-                <div class="form-group p-2">
-                    <input type="password" class="form-control" name="pass" placeholder="Password" required="required">
+                    <input type="password" class="form-control" name="password" placeholder="Password" required="required">
                 </div>
                 <div class="form-group p-2">
                     <input type="password" class="form-control" name="cpass" placeholder="Confirm Password" required="required">
@@ -36,7 +33,7 @@
                     <label class="form-check-label"><input type="checkbox" required="required"> I accept to receive emails from this organization about future events and announcements.</label>
                 </div>
                 <div class="form-group p-2">
-                    <button type="submit" name="save" class="btn btn-success btn-lg btn-block">Register Now</button>
+                    <button type="submit" name="register" class="btn btn-success btn-lg btn-block">Register Now</button>
                 </div>
                 <div class="text-center p-2">Already have an account? <a href="login.php">Sign in</a></div>
             </form>
